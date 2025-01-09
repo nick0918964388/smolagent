1d44346f72cc971e2c1344536a79599291e5479b
nickyin <nickyin@tw.ibm.com>
2025-01-09 16:29:18 +0800

    asd

--- text-to-sql/api.py
+++ text-to-sql/api.py
@@ -66,7 +66,7 @@ car_avaliable_agent = CodeAgent(
 managed_car_avaliable_agent = ManagedAgent(
     agent=car_avaliable_agent,
     name="query_car_avaliable",
-    description="查詢車輛動態或可用率相關 , 如配屬數量、借入數量、現有數量、定期數量、段修數量、待料待修數量、無火迴送數量、停用數量、備註",
+    description="查詢車輛本日可用率相關 : 如配屬數量、借入數量、現有數量、定期數量、段修數量、待料待修數量、無火迴送數量、停用數量、備註",
 )
 
 manager_agent = CodeAgent(
--- text-to-sql/sqltool.py
+++ text-to-sql/sqltool.py
@@ -83,6 +83,22 @@ def sql_engine_db2_carava(query: str) -> str:
     Allows you to perform SQL queries on the DB2 database. Returns a string representation of the result.
     The tables are:        
         Table 'ZZ_V_DAILYDYNAMIC':
+        - 車種: VARCHAR(20)
+        - 段所: VARCHAR(16)
+        - 車型: VARCHAR(16)
+        - 配屬(A): VARCHAR(16)
+        - 借出(B): VARCHAR(16)
+        - 借入(C): VARCHAR(16)
+        - 現有(D): VARCHAR(16)
+        - 定期(E): VARCHAR(16)
+        - 臨時(F): VARCHAR(16)
+        - 預備(G): VARCHAR(16)
+        - W或保養(H): VARCHAR(16)
+        - 段修(I): VARCHAR(16)
+        - 待料待修(K): VARCHAR(16)
+        - 無火迴送(L): VARCHAR(16)
+        - 停用(M): VARCHAR(16)
+        - 備註: VARCHAR(16)
     Args:
         query: The query to perform. This should be correct SQL.
     """
