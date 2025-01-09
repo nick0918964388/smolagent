e3d0f981a51cb247a7a038a785ffa452ad00b381
nickyin <nickyin@tw.ibm.com>
2025-01-09 15:34:49 +0800

    b

--- text-to-sql/api.py
+++ text-to-sql/api.py
@@ -22,12 +22,11 @@ sql_engine_db2.description = asset_description
 # sql_engine_db2.description = updated_description
 
 # 初始化模型
-model = LiteLLMModel({
-    "model_id":"ollama/qwen2.5-coder-extra:latest",
-    "api_base":"http://ollama.webtw.xyz:11434",
-    "api_key":"ollama",
-    "temperature":0.0,
-}
+model = LiteLLMModel(
+    model_id="ollama/qwen2.5-coder-extra:latest",
+    api_base="http://ollama.webtw.xyz:11434",
+    api_key="ollama",
+    {temperature=0.0}    
 )
 
 # 初始化 agent
